0d1b9f24600cd02198aedd19c2f5745787c49c8f
vincent-bekir1 <gabm54@yahoo.com>
2019-04-26 05:23:21 +0100

    Added image lazy loading

---
+++ static/js/lazy-load.js
@@ -0,0 +1,35 @@
+let lazyImages= [...document.querySelectorAll(".b-lazy")]
+let inAdvance = 300;
+function lazyLoad() {
+  // console.log(lazyImages.length)
+  lazyImages.forEach(image => {
+    if(image.offsetTop < window.innerHeight + window.pageYOffset + 300){
+      image.src = image.dataset.src;
+    }
+  })
+}
+
+lazyLoad();
+
+window.addEventListener("scroll",_.throttle(lazyLoad,50));
+window.addEventListener("resize",_.throttle(lazyLoad,50));
+
+// const option = {
+//   root: document.querySelector('.section-padding'),
+//   rootMargin: '0px 0px 200px 0px'
+// }
+// /**
+//   Function
+// **/
+// function onInterSection(imagesEntities) {
+//   imagesEntities.forEach(image => {
+//     if(image.isIntersecting){
+//       observer.unoberve(image.target);
+//       image.target.src = image.target.dataset.src;
+//     }
+//   })
+// }
+//
+// let observer = new IntersectionObserver(onInterSection, option)
+//
+// lazyImages.forEach(image => observer.observe(image));
